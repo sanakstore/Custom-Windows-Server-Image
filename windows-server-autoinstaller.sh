7f0687ae9ed1522ffbbbf93a4f63681b255883d2
sanakstore <144905341+sanakstore@users.noreply.github.com>
2024-05-11 21:39:53 +0800

    sanak store

--- windows-server-autoinstaller.sh
+++ windows-server-autoinstaller.sh
@@ -3,9 +3,11 @@
 # Function to display menu and get user choice
 display_menu() {
     echo "Please select the Windows Server version:"
-    echo "1. Windows Server 2016"
-    echo "2. Windows Server 2019"
-    echo "3. Windows Server 2022"
+    echo "1. Windows 10"
+    echo "2. Windows Server 2012 R2"
+    echo "3. Windows Server 2016"
+    echo "4. Windows Server 2019"
+    echo "5. Windows Server 2022"
     read -p "Enter your choice: " choice
 }
 
@@ -26,18 +28,30 @@ display_menu
 
 case $choice in
     1)
+        # Windows 10
+        img_file="windows10.img"
+        iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
+        iso_file="windows10.iso"
+        ;;
+    2)
+        # Windows Server 2012 R2
+        img_file="windows2012.img"
+        iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
+        iso_file="windows2012.iso"
+        ;;
+    3)
         # Windows Server 2016
         img_file="windows2016.img"
         iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
         iso_file="windows2016.iso"
         ;;
-    2)
+    4)
         # Windows Server 2019
         img_file="windows2019.img"
         iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US"
         iso_file="windows2019.iso"
         ;;
-    3)
+    5)
         # Windows Server 2022
         img_file="windows2022.img"
         iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
