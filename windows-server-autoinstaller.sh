5a08d312116124312bd5268d584ecbc595953cb3
sanakstore <144905341+sanakstore@users.noreply.github.com>
2024-05-12 12:27:59 +0800

    sanak store

--- windows-server-autoinstaller.sh
+++ windows-server-autoinstaller.sh
@@ -34,24 +34,30 @@ case $choice in
         iso_file="windows10.iso"
         ;;
     2)
+        # Windows 11
+        img_file="windows11.img"
+        iso_link="https://www.mediafire.com/file_premium/md31nzipzsb33tg/Win11_23H2_English_x64v2.iso"
+        iso_file="windows11.iso"
+        ;;
+    3)
         # Windows Server 2012 R2
         img_file="windows2012.img"
         iso_link="https://www.mediafire.com/file_premium/0q5p1bh2q94ydls/windows_server_2012_R2.ISO"
         iso_file="windows2012.iso"
         ;;
-    3)
+    4)
         # Windows Server 2016
         img_file="windows2016.img"
         iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
         iso_file="windows2016.iso"
         ;;
-    4)
+    5)
         # Windows Server 2019
         img_file="windows2019.img"
         iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US"
         iso_file="windows2019.iso"
         ;;
-    5)
+    6)
         # Windows Server 2022
         img_file="windows2022.img"
         iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
