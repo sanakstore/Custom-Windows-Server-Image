fb55fcdfad231c67f1065e2648a3691bfea86492
sanakstore <144905341+sanakstore@users.noreply.github.com>
2024-05-13 11:17:06 +0800

    sanak store

--- windows-server-autoinstaller.sh
+++ windows-server-autoinstaller.sh
@@ -7,6 +7,7 @@ display_menu() {
     echo "2. Windows Server 2016"
     echo "3. Windows Server 2019"
     echo "4. Windows Server 2022"
+    echo "5. Windows 10"
     read -p "Enter your choice: " choice
 }
 
@@ -45,6 +46,12 @@ case $choice in
         iso_file="windows2019.iso"
         ;;
     4)
+        # Windows 10
+        img_file="windows10.img"
+        iso_link="https://archive.org/download/windows-10_20H2/Win_20H2.iso"
+        iso_file="windows10.iso"
+        ;;
+    5)
         # Windows Server 2022
         img_file="windows2022.img"
         iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
